d27ef3aa8eed34cef89c64bb70be37b8b9583ff0
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-07-02 15:39:34 +0100

    goalie added, functionallity to pause game added

--- code/main.py
+++ code/main.py
@@ -1,6 +1,6 @@
 import pygame as pg
 import sys
-from sprites import Player, Goals, Ball
+from sprites import Player, Goals, Ball, Goalie
 from ui import Block, Text
 from config import (
     SCREEN_HEIGHT, SCREEN_WIDTH, BACKGROUND_IMG, FPS, BALL_PLAYER_KICK_DISTANCE
@@ -22,8 +22,8 @@ player = Player(
 goals = Goals(
     colour=(0, 255, 0),
     width=60,
-    height=30,
-    xpos=(SCREEN_WIDTH // 2) - 30,
+    height=20,
+    xpos=(SCREEN_WIDTH // 2) - 20,
     ypos=0
 )
 
@@ -34,7 +34,17 @@ ball = Ball(
     radius=10,
     width=20,
     y_change=0.75,
-    x_change=1.1
+    x_change=1.1,
+    acceleration=0.01
+)
+
+goalie = Goalie(
+    colour=(100, 50, 150),
+    width=20,
+    height=10,
+    xpos=(SCREEN_WIDTH // 2) - 5,
+    ypos=32,
+    speed=0.2
 )
 
 goals_counter = Text(
@@ -51,6 +61,7 @@ background = pg.image.load(BACKGROUND_IMG)
 def main():
 
     score = 0
+    paused = False
 
     while True:
         for event in pg.event.get():
@@ -58,33 +69,75 @@ def main():
                 pg.quit()
                 sys.exit()
 
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    if paused:
+                        paused = False
+                    else:
+                        paused = True
+
             if event.type == pg.MOUSEBUTTONDOWN:
                 if player.get_ball_player_distance(ball) < BALL_PLAYER_KICK_DISTANCE:
                     ball.kicked = True
 
-        #sprites
-        player.move(pg.mouse.get_pos())            
-        player.draw(screen)
+        if not paused:
 
-        if ball.kicked:
-            if goals.has_scored(ball):
-                score += 1
-                goals_counter.update_text(f'Score : {score}')
+            #sprites
+            player.move(pg.mouse.get_pos())            
+            player.draw(screen)
 
-        if ball.display:
-            ball.draw(screen)
-            
-        if ball.kicked:
-            ball.travel_to_goal()
+            if ball.kicked:
+                if goals.has_scored(ball):
+                    score += 1
+                    goals_counter.update_text(f'Score : {score}')
 
-        ball.move()
+            if ball.display:
+                ball.draw(screen)
+                
+            if ball.kicked:
+                ball.travel_to_goal()
 
-        #ui
-        goals_counter.blit(screen)
+            ball.move()
 
-        clock.tick(FPS)
-        pg.display.update()
-        screen.blit(background, (0,0))
+            goalie.draw(screen)
+            goalie.move(ball, goals)
+
+            if ball.kicked:
+                if goalie.shot_saved(ball):
+                    if ball.xpos <= goalie.get_center()[0] // 2:
+                        ball.x_change = 1
+                    else:
+                        ball.x_change = -1
+
+                    ball.y_change = 1
+                    ball.acceleration = -0.001
+                    ball.kicked = False
+
+            #ui
+            goals_counter.blit(screen)
+
+            clock.tick(FPS)
+            pg.display.update()
+            screen.blit(background, (0,0))
+
+        else:
+            exit_button = Block(
+                colour=(255, 0, 0),
+                xpos=SCREEN_WIDTH // 2 + 50,
+                ypos=SCREEN_HEIGHT // 2 + 20,
+                width=100,
+                height=40
+            )
+            exit_text = Text(
+                colour=(255, 100, 150),
+                xpos=SCREEN_WIDTH // 2 + 50,
+                ypos=SCREEN_HEIGHT // 2 + 20,
+                text='EXIT',
+                font_size=15
+            )
+
+            exit_button.draw(screen)
+            exit_text.blit(screen)
 
 
 if __name__ == '__main__':
